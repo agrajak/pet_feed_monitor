26fa20c477bd31abef5d1ba9b7ee7981ccefc744
Agrajak <korbots@gmail.com>
2019-06-02 19:30:25 +0900

    TWI구현 (테스트)

--- pet_feed_monitor.ino
+++ pet_feed_monitor.ino
@@ -1,5 +1,13 @@
 unsigned char data;
 #define COM_HELLO 1
+#define ADDR_DS1307 0b1101000
+
+char rBuf[30];
+char wBuf[50];
+char rcnt, wcnt;
+int cnt;
+
+
 void initLEDs(){
   DDRD |= (1 << DDD5) | (1 << DDD6) | (1 << DDD7); // make LED1, LED2 OUTPUT
   DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4);
@@ -55,10 +63,89 @@ void initUART(){
 
   // Transmit and Recievie Examples on 186page
 }
-char rBuf[30];
-char wBuf[50];
-char rcnt, wcnt;
-int cnt;
+
+void initTWI(){
+  // 메뉴얼 225쪽 참조
+  // DS1307 Slave Address = 1101000(2)
+
+  // Master Transmiter
+  // After Start Condition has been transmitted, TWSR will be 0xF8;
+  
+
+  // SCL freq =  Fcpu / 16 + 2(TWBR)(PresclaeValue)
+
+  TWBR = 16;
+  TWSR = TWSR | 3;
+  TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN); 
+  LED1(1);
+  while(!(TWCR & (1<<TWINT)));
+  if((TWSR & 0xF8) != 0x08){
+    TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+    initTWI();
+    return;
+  }
+  LED2(1);
+  // switch(TWSR & 0xF8){
+  //   case MT_SLA_ACK:
+  //     break;
+  //   default:
+  //     TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO); // send stop signal!
+  //     debugValue(90);
+  // }
+  // Send SLA+W(W=0, R=1)
+  TWDR = ADDR_DS1307 << 1 + 0;
+  // TWINT should be cleared
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  // Wait for TWINT Flag set.
+  while (!(TWCR & (1<<TWINT)));
+
+  if((TWSR & 0xF8) != 0x18){ // -> 227, 230page
+    debugValue(TWSR);
+  //  TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+//    initTWI();
+    return;
+  }
+  int s, m, h, d, M, Y;
+  int ch;
+
+  LED3(1);
+  // send data;
+  TWDR = 0x00;
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  while(!(TWCR & (1<<TWINT)));
+
+  TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTA);
+  while(!(TWCR & (1<<TWINT)));
+
+  TWDR = ADDR_DS1307 << 1 + 1;
+  // TWINT should be cleared
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  // Wait for TWINT Flag set.
+  while (!(TWCR & (1<<TWINT)));
+  ch = (TWDR & 0x80) >> 7;
+  s = (TWDR & 0x0F) + ((TWDR & 0x70) >> 4) * 10;
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  while(!(TWCR & (1<<TWINT)));
+//  debugValue(7);
+
+  m = (TWDR & 0x0F) + ((TWDR & 0x70) >> 4) * 10;
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  while(!(TWCR & (1<<TWINT)));
+//  debugValue(8);
+
+  h = (TWDR & 0x0F) + ((TWDR & 0x10) >> 4) * 10;
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  // wait
+  while(!(TWCR & (1<<TWINT)));
+//  debugValue(9);
+  // send stop;
+  TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+  LED5(1);
+
+  sprintf(wBuf, "ch=%d,%d:%d:%d", ch, h,m,s);
+  respond(wBuf);
+
+}
 int verifyCommand();
 int verifyCommand(){
   if(rBuf[0]=='H' && rBuf[1]=='I'){
@@ -98,6 +185,7 @@ void do_command(){
 void setup() {
   initLEDs();
   initUART();
+  initTWI();
 }
 ISR(USART_RX_vect){
 }
@@ -107,7 +195,7 @@ void loop() {
 
   while(!(UCSR0A & (1<<RXC0))); // wait until RX will be available!
   rBuf[rcnt++] = UDR0;
-  debugValue(rcnt);
+//  debugValue(rcnt);
 
   do_command();
 }
