011f0ad25e4083b81ec48ff503971ad0d6e5bf32
agrajak <korbots@gmail.com>
2019-06-03 13:09:47 +0900

    에러코드 수정 (그러나 미작동)

--- pet_feed_monitor.ino
+++ pet_feed_monitor.ino
@@ -1,6 +1,7 @@
 unsigned char data;
 #define COM_HELLO 1
 #define ADDR_DS1307 0b1101000
+#define ADDR_24LC02B 0b1010000
 
 char rBuf[30];
 char wBuf[50];
@@ -63,7 +64,69 @@ void initUART(){
 
   // Transmit and Recievie Examples on 186page
 }
+unsigned char SLAVE_ADDR;
+void setSlaveAddr(unsigned char addr){
+  SLAVE_ADDR = addr;
+}
+unsigned char readTWI(unsigned char addr){
+  unsigned char data;
+  startTWI();  // ====> START
+  if((TWSR & 0xF8) != 0x08) { debugValue(TWSR & 0xF8); return; }
+  
+  sendSLARW(0);   // ====> SLA+W
+  if((TWSR & 0xF8) != 0x18) { debugValue(TWSR & 0xF8); return; }
+  
+  TWDR = addr; 
+  clearTWCR(); // ====> WORD ADDRESS
+  if((TWSR & 0xF8) != 0x28) { debugValue(TWSR & 0xF8); return; }
 
+  startTWI();  // ====> RE-START
+  if((TWSR & 0xF8) != 0x10) { debugValue(TWSR & 0xF8); return; }
+  sendSLARW(1);   // ====> SLA+R
+  if((TWSR & 0xF8) != 0x40) { debugValue(TWSR & 0xF8); return; }
+  
+  data = TWDR; 
+  clearTWCR(); // ====> WORD ADDRESS
+  if((TWSR & 0xF8) != 0x50) { debugValue(TWSR & 0xF8); return; }
+
+  stopTWI();
+  debugValue(0xFF);
+  return data;
+}
+unsigned char writeTWI(unsigned char addr, unsigned char data){
+  startTWI();  // ====> START
+  if((TWSR & 0xF8) != 0x08) { debugValue(TWSR & 0xF8); return; }
+
+  sendSLARW(0);   // ====> SLA+W
+  if((TWSR & 0xF8) != 0x18) { debugValue(TWSR & 0xF8); return; }
+  
+  TWDR = addr; 
+  clearTWCR(); // ====> WORD ADDRESS
+  if((TWSR & 0xF8) != 0x28) { debugValue(TWSR & 0xF8); return; }
+
+  TWDR = data; 
+  clearTWCR(); // ====> WORD ADDRESS
+  if((TWSR & 0xF8) != 0x28) { debugValue(TWSR & 0xF8); return; }
+
+  stopTWI();  
+  debugValue(0xFF);
+}
+void startTWI(){
+  TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN); 
+  while(!(TWCR & (1<<TWINT)));
+}
+void stopTWI(){
+  TWCR = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN); 
+}
+void sendSLARW(int mode){
+  TWDR = (SLAVE_ADDR << 1) + mode;
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  while (!(TWCR & (1<<TWINT)));
+}
+void clearTWCR(){
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  while (!(TWCR & (1<<TWINT)));
+}
 void initTWI(){
   // 메뉴얼 225쪽 참조
   // DS1307 Slave Address = 1101000(2)
@@ -71,80 +134,111 @@ void initTWI(){
   // Master Transmiter
   // After Start Condition has been transmitted, TWSR will be 0xF8;
   
+  // SCL freq =  16000khz / 16 + 2(TWBR)(PresclaeValue)
 
-  // SCL freq =  Fcpu / 16 + 2(TWBR)(PresclaeValue)
+  TWBR = 32;
+  TWSR = TWSR | 0b10;
+  
 
-  TWBR = 16;
-  TWSR = TWSR | 3;
-  TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN); 
-  LED1(1);
-  while(!(TWCR & (1<<TWINT)));
+  /*
+  startTWI();
   if((TWSR & 0xF8) != 0x08){
-    TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
     initTWI();
     return;
   }
-  LED2(1);
-  // switch(TWSR & 0xF8){
-  //   case MT_SLA_ACK:
-  //     break;
-  //   default:
-  //     TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO); // send stop signal!
-  //     debugValue(90);
-  // }
-  // Send SLA+W(W=0, R=1)
-  TWDR = ADDR_DS1307 << 1 + 0;
-  // TWINT should be cleared
-  TWCR = (1<<TWINT) | (1<<TWEN);
-  // Wait for TWINT Flag set.
-  while (!(TWCR & (1<<TWINT)));
+  LED1(1);
 
-  if((TWSR & 0xF8) != 0x18){ // -> 227, 230page
-    debugValue(TWSR);
-  //  TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-//    initTWI();
+  sendSLARW(0b1101000, 0);
+  if((TWSR & 0xF8) != 0x18){ // SLA+W, ACK rcieved!
+    initTWI();
     return;
   }
+  LED1(2);
+
   int s, m, h, d, M, Y;
   int ch;
 
-  LED3(1);
   // send data;
+
+  TWDR = 0x00;
+  clearTWCR();
+  LED1(3);
+
+  if((TWSR & 0xF8) != 0x28){ // Data + ACK!
+ //   debugValue(TWSR);
+    initTWI();
+    return;
+  }
+  TWDR = 0x00;
+  clearTWCR();
+  if((TWSR & 0xF8) != 0x28){
+ //   debugValue(TWSR);
+    initTWI();
+    return;
+  }
+  LED1(4);
+
+  stopTWI();
+  respond("WRITING 00 to 00 complete!");
+  debugValue(TWCR);
+
+  LED2(1);
+
+  startTWI();
+  if((TWSR & 0xF8) != 0x08){ // check repeated START condition!
+  //  debugValue(TWSR);
+    initTWI();
+    return;
+  }
+  LED3(1);
+  sendSLARW(0b1101000, 0);
+
   TWDR = 0x00;
   TWCR = (1<<TWINT) | (1<<TWEN);
-  while(!(TWCR & (1<<TWINT)));
+  while (!(TWCR & (1<<TWINT)));
 
   TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTA);
   while(!(TWCR & (1<<TWINT)));
 
-  TWDR = ADDR_DS1307 << 1 + 1;
-  // TWINT should be cleared
+  TWDR = 0b11010001;
   TWCR = (1<<TWINT) | (1<<TWEN);
-  // Wait for TWINT Flag set.
   while (!(TWCR & (1<<TWINT)));
-  ch = (TWDR & 0x80) >> 7;
-  s = (TWDR & 0x0F) + ((TWDR & 0x70) >> 4) * 10;
-  TWCR = (1<<TWINT) | (1<<TWEN);
-  while(!(TWCR & (1<<TWINT)));
-//  debugValue(7);
 
-  m = (TWDR & 0x0F) + ((TWDR & 0x70) >> 4) * 10;
-  TWCR = (1<<TWINT) | (1<<TWEN);
-  while(!(TWCR & (1<<TWINT)));
-//  debugValue(8);
+  if((TWSR & 0xF8) != 0x40){ // SLA+W -> ACK!
+    debugValue(TWSR);
+    initTWI();
+    return;
+  }
 
-  h = (TWDR & 0x0F) + ((TWDR & 0x10) >> 4) * 10;
-  TWCR = (1<<TWINT) | (1<<TWEN);
-  // wait
-  while(!(TWCR & (1<<TWINT)));
-//  debugValue(9);
-  // send stop;
-  TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
-  LED5(1);
+//  LED4(1);
+  data = TWDR;
+  ch = (data & 0x80) >> 7;
+  s = (data & 0x0F) + ((data & 0x70) >> 4) * 10;
+  int sr = data;
+  clearTWCR();
+
+  data = TWDR;
+  m = (data & 0x0F) + ((data & 0x70) >> 4) * 10;
+  int mr = data;
+  clearTWCR();
+//  LED5(1);
+
+  data = TWDR;
+  h = (data & 0x0F) + ((data & 0x10) >> 4) * 10;
+  int hr = data;
+  clearTWCR();
+
+
+  data = TWDR;
+  int dr = data;
+  clearTWCR();
+  stopTWI();
+//  LED6(1);
 
   sprintf(wBuf, "ch=%d,%d:%d:%d", ch, h,m,s);
   respond(wBuf);
-
+  debugValue(sr);
+  */
 }
 int verifyCommand();
 int verifyCommand(){
@@ -186,6 +280,26 @@ void setup() {
   initLEDs();
   initUART();
   initTWI();
+
+  setSlaveAddr(ADDR_24LC02B);
+  writeTWI(0x00, 12);
+  writeTWI(0x01, 21);
+
+  unsigned char a = readTWI(0x00);
+  sprintf(wBuf, "a is %x", a);
+  respond(wBuf);
+  a = readTWI(0x00);
+  sprintf(wBuf, "a2 is %x", a);
+  respond(wBuf);
+  a = readTWI(0x00);
+  sprintf(wBuf, "a3 is %x", a);
+  respond(wBuf);
+  unsigned char b = readTWI(0x01);
+
+  sprintf(wBuf, "b is %x", b);
+  respond(wBuf);
+
+  
 }
 ISR(USART_RX_vect){
 }
