4556f155af78c1a87d450f325959fde099e54d9b
Agrajak <korbots@gmail.com>
2019-06-05 00:36:43 +0900

    TWI READ 성공!

--- pet_feed_monitor.ino
+++ pet_feed_monitor.ino
@@ -1,43 +1,33 @@
-unsigned char data;
+
 #define COM_HELLO 1
+#define COM_INIT_CLOCK 2
+#define COM_SET_CLOCK 3
+#define COM_CURRENT_CLOCK 4
+
 #define ADDR_DS1307 0b1101000
 #define ADDR_24LC02B 0b1010000
+
+uint8_t data;
 
 char rBuf[30];
 char wBuf[50];
 char rcnt, wcnt;
 int cnt;
 
-
 void initLEDs(){
   DDRD |= (1 << DDD5) | (1 << DDD6) | (1 << DDD7); // make LED1, LED2 OUTPUT
   DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4);
 }
-void LED1(int a){
-  PORTD = (PORTD & ~(1<<PORTD6)) | (a<<PORTD6);
-}
-void LED2(int a){
-  PORTD = (PORTD & ~(1<<PORTD7)) | (a<<PORTD7);
-}
-void LED3(int a){
-  PORTB = (PORTB & ~(1<<PORTB0)) | (a<<PORTB0);
-}
-void LED4(int a){
-  PORTD = (PORTD & ~(1<<PORTD5)) | (a<<PORTD5);
-}
-void LED5(int a){
-  PORTB = (PORTB & ~(1<<PORTB1)) | (a<<PORTB1);
-}
-void LED6(int a){
-  PORTB = (PORTB & ~(1<<PORTB2)) | (a<<PORTB2);
-}
-void LED7(int a){
-  PORTB = (PORTB & ~(1<<PORTB3)) | (a<<PORTB3);
-}
-void LED8(int a){
-  PORTB = (PORTB & ~(1<<PORTB4)) | (a<<PORTB4);
-}
-void debugValue(char v){
+void LED1(int a){ PORTD = (PORTD & ~(1<<PORTD6)) | (a<<PORTD6); }
+void LED2(int a){ PORTD = (PORTD & ~(1<<PORTD7)) | (a<<PORTD7); }
+void LED3(int a){ PORTB = (PORTB & ~(1<<PORTB0)) | (a<<PORTB0); }
+void LED4(int a){ PORTD = (PORTD & ~(1<<PORTD5)) | (a<<PORTD5); }
+void LED5(int a){ PORTB = (PORTB & ~(1<<PORTB1)) | (a<<PORTB1); }
+void LED6(int a){ PORTB = (PORTB & ~(1<<PORTB2)) | (a<<PORTB2); }
+void LED7(int a){ PORTB = (PORTB & ~(1<<PORTB3)) | (a<<PORTB3); }
+void LED8(int a){ PORTB = (PORTB & ~(1<<PORTB4)) | (a<<PORTB4); }
+
+void debugValue(uint8_t v){
   LED1(v & 1);
   LED2((v & (1<<1)) >> 1);
   LED3((v & (1<<2)) >> 2);
@@ -50,8 +40,8 @@ void debugValue(char v){
 
 void initUART(){
 //  // USART INIT (Manual 179Page)
-  UBRR0H = (unsigned char) (103>>8);
-  UBRR0L = (unsigned char) (103);
+  UBRR0H = (uint8_t) (103>>8);
+  UBRR0L = (uint8_t) (103);
     
   // Fosc = 16.0Mhz, BaudRate = 38.4kbps, UBRRn => 25(U2Xn=0) / 51(U2Xn=1)
   // U2Xn=0 -> Normal mode, U2Xn=1-> Double Speed mode!
@@ -64,49 +54,61 @@ void initUART(){
 
   // Transmit and Recievie Examples on 186page
 }
-unsigned char SLAVE_ADDR;
-void setSlaveAddr(unsigned char addr){
+uint8_t SLAVE_ADDR;
+void setSlaveAddr(uint8_t addr){
   SLAVE_ADDR = addr;
+  sprintf(wBuf, "SLA+W : %x", (addr<<1));
+  respond(wBuf);
+  sprintf(wBuf, "SLA+R : %x", (addr<<1)+1);
+  respond(wBuf);
 }
-unsigned char readTWI(unsigned char addr){
-  unsigned char data;
+void sendSLARW(int mode){
+  TWDR = (SLAVE_ADDR << 1) + mode;
+  TWCR = (1<<TWINT) | (1<<TWEN);
+  while (!(TWCR & (1<<TWINT)));
+}
+
+uint8_t readTWI(uint8_t addr){
+  uint8_t data;
   startTWI();  // ====> START
-  if((TWSR & 0xF8) != 0x08) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x08) { debugValue(1); return (TWSR & 0xF8); }
   
   sendSLARW(0);   // ====> SLA+W
-  if((TWSR & 0xF8) != 0x18) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x18) { debugValue(2); stopTWI(); return (TWSR & 0xF8); }
   
   TWDR = addr; 
   clearTWCR(); // ====> WORD ADDRESS
-  if((TWSR & 0xF8) != 0x28) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x28) { debugValue(3); stopTWI();  return (TWSR & 0xF8); }
 
   startTWI();  // ====> RE-START
-  if((TWSR & 0xF8) != 0x10) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x10) { debugValue(4); stopTWI();  return (TWSR & 0xF8); }
+
   sendSLARW(1);   // ====> SLA+R
-  if((TWSR & 0xF8) != 0x40) { debugValue(TWSR & 0xF8); return; }
-  
+  if((TWSR & 0xF8) != 0x40) { debugValue(5); stopTWI();  return (TWSR & 0xF8); }
+  clearTWCR(); 
   data = TWDR; 
+
   clearTWCR(); // ====> WORD ADDRESS
-  if((TWSR & 0xF8) != 0x50) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x50 && (TWSR & 0xF8) != 0x58) { debugValue(6); stopTWI();  return (TWSR & 0xF8); }
 
   stopTWI();
   debugValue(0xFF);
   return data;
 }
-unsigned char writeTWI(unsigned char addr, unsigned char data){
+uint8_t writeTWI(uint8_t addr, uint8_t data){
   startTWI();  // ====> START
-  if((TWSR & 0xF8) != 0x08) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x08) { debugValue(7); return (TWSR & 0xF8); }
 
   sendSLARW(0);   // ====> SLA+W
-  if((TWSR & 0xF8) != 0x18) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x18) { debugValue(8); stopTWI();  return (TWSR & 0xF8); }
   
   TWDR = addr; 
   clearTWCR(); // ====> WORD ADDRESS
-  if((TWSR & 0xF8) != 0x28) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x28) { debugValue(9); stopTWI();  return (TWSR & 0xF8); }
 
   TWDR = data; 
   clearTWCR(); // ====> WORD ADDRESS
-  if((TWSR & 0xF8) != 0x28) { debugValue(TWSR & 0xF8); return; }
+  if((TWSR & 0xF8) != 0x28) { debugValue(10); stopTWI();  return (TWSR & 0xF8); }
 
   stopTWI();  
   debugValue(0xFF);
@@ -117,11 +119,7 @@ void startTWI(){
 }
 void stopTWI(){
   TWCR = (1<<TWINT) | (1<<TWSTO) | (1<<TWEN); 
-}
-void sendSLARW(int mode){
-  TWDR = (SLAVE_ADDR << 1) + mode;
-  TWCR = (1<<TWINT) | (1<<TWEN);
-  while (!(TWCR & (1<<TWINT)));
+  while(!(TWCR & (1<<TWSTO)));
 }
 void clearTWCR(){
   TWCR = (1<<TWINT) | (1<<TWEN);
@@ -132,118 +130,29 @@ void initTWI(){
   // DS1307 Slave Address = 1101000(2)
 
   // Master Transmiter
-  // After Start Condition has been transmitted, TWSR will be 0xF8;
   
   // SCL freq =  16000khz / 16 + 2(TWBR)(PresclaeValue)
 
-  TWBR = 32;
-  TWSR = TWSR | 0b10;
-  
+  // 16 + 2TWBR = 160
+  // Target : SCL : 100Khz
+  // TWBR = 72
 
-  /*
-  startTWI();
-  if((TWSR & 0xF8) != 0x08){
-    initTWI();
-    return;
-  }
-  LED1(1);
-
-  sendSLARW(0b1101000, 0);
-  if((TWSR & 0xF8) != 0x18){ // SLA+W, ACK rcieved!
-    initTWI();
-    return;
-  }
-  LED1(2);
-
-  int s, m, h, d, M, Y;
-  int ch;
-
-  // send data;
-
-  TWDR = 0x00;
-  clearTWCR();
-  LED1(3);
-
-  if((TWSR & 0xF8) != 0x28){ // Data + ACK!
- //   debugValue(TWSR);
-    initTWI();
-    return;
-  }
-  TWDR = 0x00;
-  clearTWCR();
-  if((TWSR & 0xF8) != 0x28){
- //   debugValue(TWSR);
-    initTWI();
-    return;
-  }
-  LED1(4);
-
-  stopTWI();
-  respond("WRITING 00 to 00 complete!");
-  debugValue(TWCR);
-
-  LED2(1);
-
-  startTWI();
-  if((TWSR & 0xF8) != 0x08){ // check repeated START condition!
-  //  debugValue(TWSR);
-    initTWI();
-    return;
-  }
-  LED3(1);
-  sendSLARW(0b1101000, 0);
-
-  TWDR = 0x00;
-  TWCR = (1<<TWINT) | (1<<TWEN);
-  while (!(TWCR & (1<<TWINT)));
-
-  TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTA);
-  while(!(TWCR & (1<<TWINT)));
-
-  TWDR = 0b11010001;
-  TWCR = (1<<TWINT) | (1<<TWEN);
-  while (!(TWCR & (1<<TWINT)));
-
-  if((TWSR & 0xF8) != 0x40){ // SLA+W -> ACK!
-    debugValue(TWSR);
-    initTWI();
-    return;
-  }
-
-//  LED4(1);
-  data = TWDR;
-  ch = (data & 0x80) >> 7;
-  s = (data & 0x0F) + ((data & 0x70) >> 4) * 10;
-  int sr = data;
-  clearTWCR();
-
-  data = TWDR;
-  m = (data & 0x0F) + ((data & 0x70) >> 4) * 10;
-  int mr = data;
-  clearTWCR();
-//  LED5(1);
-
-  data = TWDR;
-  h = (data & 0x0F) + ((data & 0x10) >> 4) * 10;
-  int hr = data;
-  clearTWCR();
-
-
-  data = TWDR;
-  int dr = data;
-  clearTWCR();
-  stopTWI();
-//  LED6(1);
-
-  sprintf(wBuf, "ch=%d,%d:%d:%d", ch, h,m,s);
-  respond(wBuf);
-  debugValue(sr);
-  */
+  TWBR = 72;
+  TWSR = 0;
 }
 int verifyCommand();
 int verifyCommand(){
   if(rBuf[0]=='H' && rBuf[1]=='I'){
     return COM_HELLO;
+  }
+  if(rBuf[0]=='C' && rBuf[1]=='T'){
+    return COM_CURRENT_CLOCK;
+  }
+  if(rBuf[0]=='I' && rBuf[1]=='T'){
+    return COM_INIT_CLOCK;
+  }
+  if(rBuf[0]=='S' && rBuf[1]=='T'){
+    return COM_SET_CLOCK;
   }
   return NULL;
 }
@@ -270,46 +179,70 @@ void do_command(){
       case COM_HELLO:
         respond("Hello!");
         break;
-      case NULL:
+      case COM_CURRENT_CLOCK:
+        
+        break;
+      case COM_SET_CLOCK:
+        
+        break;
+      case COM_INIT_CLOCK:
+        break;
+      default:
         respond("Undeclared Command!");
         break;
     }
   }
 }
-void setup() {
+int main(){
   initLEDs();
   initUART();
   initTWI();
 
-  setSlaveAddr(ADDR_24LC02B);
-  writeTWI(0x00, 12);
-  writeTWI(0x01, 21);
+//  setSlaveAddr(ADDR_DS1307);
+  setSlaveAddr(ADDR_DS1307);
+/*
+  uint8_t data=0x23;
 
-  unsigned char a = readTWI(0x00);
+  startTWI();  // ====> START
+  if((TWSR & 0xF8) != 0x08) { debugValue(1); return (TWSR & 0xF8); }
+  
+  sendSLARW(0);   // ====> SLA+W
+  if((TWSR & 0xF8) != 0x18) { debugValue(2); stopTWI(); return (TWSR & 0xF8); }
+  
+  TWDR = 0x00; 
+  clearTWCR(); // ====> WORD ADDRESS
+  if((TWSR & 0xF8) != 0x28) { debugValue(3); stopTWI();  return (TWSR & 0xF8); }
+
+  startTWI();  // ====> RE-START
+  if((TWSR & 0xF8) != 0x10) { debugValue(4); stopTWI();  return (TWSR & 0xF8); }
+
+  sendSLARW(1);   // ====> SLA+R
+  if((TWSR & 0xF8) != 0x40) { debugValue(5); stopTWI();  return (TWSR & 0xF8); }
+  clearTWCR();
+
+  data = TWDR; 
+  clearTWCR(); // ====> WORD ADDRESS
+  if((TWSR & 0xF8) != 0x50 && (TWSR & 0xF8) != 0x58) { debugValue(6); stopTWI();  return (TWSR & 0xF8); }
+  debugValue(12);
+
+  stopTWI();
+  debugValue(data);
+  sprintf(wBuf, "data is %x!", data);
+  respond(wBuf);
+  
+*/
+  uint8_t a = readTWI(0x00);
   sprintf(wBuf, "a is %x", a);
   respond(wBuf);
-  a = readTWI(0x00);
-  sprintf(wBuf, "a2 is %x", a);
-  respond(wBuf);
-  a = readTWI(0x00);
-  sprintf(wBuf, "a3 is %x", a);
-  respond(wBuf);
-  unsigned char b = readTWI(0x01);
-
+  
+  uint8_t b = readTWI(0x01);
   sprintf(wBuf, "b is %x", b);
-  respond(wBuf);
-
+  respond(wBuf); 
   
 }
-ISR(USART_RX_vect){
-}
-ISR(USART_TX_vect){
-}
-void loop() {
+// void loop() {
 
-  while(!(UCSR0A & (1<<RXC0))); // wait until RX will be available!
-  rBuf[rcnt++] = UDR0;
-//  debugValue(rcnt);
-
-  do_command();
-}
+  // while(!(UCSR0A & (1<<RXC0))); // wait until RX will be available!
+  // rBuf[rcnt++] = UDR0;
+  // do_command();
+// }
